ab84a90f26fe00ea8503d1bda76284d4d597648e
Obed Mulumba <ommartiste007@gmail.com>
2022-03-04 08:51:42 +0000

    update files

--- database/migrations/2022_03_04_085055_create_posts_table.php
+++ database/migrations/2022_03_04_085055_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
