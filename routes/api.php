ca854904c9a384ac963a72030e1ec15d67aeb460
Obed Mulumba <ommartiste007@gmail.com>
2022-03-04 08:57:12 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('posts', 'PostController@index');
+Route::group(['prefix' => 'post'], function () {
+    Route::post('add', 'PostController@add');
+    Route::get('edit/{id}', 'PostController@edit');
+    Route::post('update/{id}', 'PostController@update');
+    Route::delete('delete/{id}', 'PostController@delete');
+});
