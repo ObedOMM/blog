b524dc5119b0e2c21b94fe06db4389d8aae16cf9
Obed Mulumba <ommartiste007@gmail.com>
2022-03-04 08:56:38 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('{any}', function () {
+    return view('app');
+})->where('any', '.*');
